cb618df466afdf5eb6509bac307609feb3cf1454
Ferney Estupinan Baron <ferney.estupinanb@gmail.com>
2024-05-03 21:36:56 -0500

    Init

---
+++ src/main/java/com/fbaron/MyServlet.java
@@ -0,0 +1,26 @@
+package com.fbaron;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author Ferney Estupinan Baron
+ */
+public class MyServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // Generate and send response
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html><body>");
+        out.println("<h1>Hello, from MyServlet</h1>");
+        out.println("</body></html>");
+    }
+
+}
