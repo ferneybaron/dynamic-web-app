8576deac101ddac50c820937d0290897858c4f81
Ferney Estupinan Baron <ferney.estupinanb@gmail.com>
2024-06-17 02:24:59 -0500

    Migrated from: * Java EE to Jakarta EE. * Java 1.8 to Java 17. * Tomcat 8 to Tomcat 10.

--- src/main/java/com/fbaron/controller/LoginServletController.java
+++ src/main/java/com/fbaron/controller/LoginServletController.java
@@ -2,14 +2,13 @@ package com.fbaron.controller;
 
 import com.fbaron.dao.UserDAOImpl;
 import com.fbaron.model.UserModel;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.sql.*;
 
 /**
  * @author Ferney Estupinan Baron
--- src/main/java/com/fbaron/jdbc/interfaces/PreparedStatementInterface.java
+++ src/main/java/com/fbaron/jdbc/interfaces/PreparedStatementInterface.java
@@ -1,6 +1,9 @@
 package com.fbaron.jdbc.interfaces;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 /**
  * @author Ferney Estupinan Baron
