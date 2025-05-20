11dc75701e65127355090c95f93c0be438892d5b
Ferney Estupinan Baron <ferneyestupinan.option@latam.com>
2025-05-19 22:58:31 -0500

    Added Hikari Connection Pool

--- src/main/java/com/fbaron/model/CustomerModel.java
+++ src/main/java/com/fbaron/model/CustomerModel.java
@@ -16,4 +16,73 @@ public class CustomerModel {
 
 
     private List<CustomerProductModel> products;
+
+
+    public CustomerModel() {
+    }
+
+    public CustomerModel(Long id, UserModel user, String email, String phone, String address, String status) {
+        this.id = id;
+        this.user = user;
+        this.email = email;
+        this.phone = phone;
+        this.address = address;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public UserModel getUser() {
+        return user;
+    }
+
+    public void setUser(UserModel user) {
+        this.user = user;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public List<CustomerProductModel> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<CustomerProductModel> products) {
+        this.products = products;
+    }
 }
--- src/main/java/com/fbaron/model/CustomerProductModel.java
+++ src/main/java/com/fbaron/model/CustomerProductModel.java
@@ -16,4 +16,74 @@ public class CustomerProductModel {
     private LocalDateTime createdDate;
     private String status;
 
+    public CustomerProductModel() {
+    }
+
+    public CustomerProductModel(Long id, CustomerModel customer, ProductModel product, String accountNumber,
+                                BigDecimal balance, LocalDateTime createdDate, String status) {
+        this.id = id;
+        this.customer = customer;
+        this.product = product;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+        this.createdDate = createdDate;
+        this.status = status;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public CustomerModel getCustomer() {
+        return customer;
+    }
+
+    public void setCustomer(CustomerModel customer) {
+        this.customer = customer;
+    }
+
+    public ProductModel getProduct() {
+        return product;
+    }
+
+    public void setProduct(ProductModel product) {
+        this.product = product;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public BigDecimal getBalance() {
+        return balance;
+    }
+
+    public void setBalance(BigDecimal balance) {
+        this.balance = balance;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 }
